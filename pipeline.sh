210dac6cc84358db7c2d7f4d8e590f1bc42ef74c
Juan Arias Del Angel <jariasdelangel@gmail.com>
2017-10-17 20:04:05 -0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -7,3 +7,9 @@ blastp -db outfile.fasta -query TBP_seed.fasta -out outfile_blast.out -outfmt 6 
 more outfile_blast.out | cut -f2 | sort | uniq > outfile_filtered.csv
 
 # run R merging_results.r
+
+perl /home/jarias/Escritorio/filter_database.pl outfile.fasta outfile_filtered.csv > tbp_filtered.fasta
+
+makeblastdb -in tbp_filtered.fasta -dbtype prot
+
+blastp -db tbp_filtered.fasta -query tbp_filtered.fasta -out outfile_blast.out -outfmt 6 -evalue 1e-50
